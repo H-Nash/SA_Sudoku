131e0b762e7c898cfc25bc018bc302d93abac5d8
EricDiGi <etd7@students.uwf.edu>
2022-04-19 14:38:48 -0500

    Linear solution works

--- LinearProgram.py
+++ LinearProgram.py
@@ -13,55 +13,49 @@ class Linear:
         self.problem.setObjective(self.objective)
 
         self.set_constraints(game.getBoard(), self.problem, self.grid_v, self.rows, self.cols, self.grids, self.values)
+        self.add_prefilled_constraints(self.problem, game.getBoard(), self.grid_v, self.rows, self.cols, self.values)
         pass
 
     def solve(self):
         self.problem.solve()
-
         status = plp.LpStatus[self.problem.status]
         print(f'Solution Status = {plp.LpStatus[self.problem.status]}')
 
-        #if status == "Optimal":
-        self.solution = self.collect(self.grid_v, self.rows, self.cols, self.values)
+        if status == "Optimal":
+            self.solution = self.collect(self.grid_v, self.rows, self.cols, self.values)
 
     def apply_solution(self):
         self.input_game.apply_solution(self.solution)
+    
+    def add_prefilled_constraints(self, prob, input_sudoku, grid_vars, rows, cols, values):
+        for row in rows:
+            for col in cols:
+                if(input_sudoku[row][col] != 0):
+                    prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_vars[row][col][value]*value  for value in values]), 
+                                                        sense=plp.LpConstraintEQ, 
+                                                        rhs=input_sudoku[row][col],
+                                                        name=f"constraint_prefilled_{row}_{col}"))
 
-    def set_constraints(self, game, prob, grid, rows, cols, grids, values):
-        #One value in cell
-        for r in rows:
-            for c in cols:
-                prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid[r][c][v] for v in values]),
-                    sense=plp.LpConstraintEQ, rhs=1, name=f"constraint_sum_{r}_{c}"))
+    def set_constraints(self, game, prob, grid_vars, rows, cols, grids, values):
+        for row in rows:
+            for col in cols:
+                    prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_vars[row][col][value] for value in values]),
+                                            sense=plp.LpConstraintEQ, rhs=1, name=f"constraint_sum_{row}_{col}"))    
+        for row in rows:
+            for value in values:
+                prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_vars[row][col][value]*value  for col in cols]),
+                                            sense=plp.LpConstraintEQ, rhs=value, name=f"constraint_uniq_row_{row}_{value}"))      
+        for col in cols:
+            for value in values:
+                prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_vars[row][col][value]*value  for row in rows]),
+                                            sense=plp.LpConstraintEQ, rhs=value, name=f"constraint_uniq_col_{col}_{value}"))     
+        for grid in grids:
+            grid_row  = int(grid/3)
+            grid_col  = int(grid%3)
 
-
-        # Row only 1-9       
-        for r in rows:
-            for v in values:
-                prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid[r][c][v]*v for c in cols]),
-                    sense=plp.LpConstraintEQ, rhs=v, name=f"constraint_uniq_row_{r}_{v}"))
-
-        # Col only 1-9     
-        for c in cols:
-            for v in values:
-                prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid[r][c][v]*v  for r in rows]),
-                    sense=plp.LpConstraintEQ, rhs=v, name=f"constraint_uniq_col_{c}_{v}"))
-
-
-        # Fill 3x3 Block once  
-        for g in grids:
-            g_r  = int(g/3) #row
-            g_c  = int(g%3) #col
-
-            for v in values:
-                prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid[g_r*3+r][g_c*3+c][v]*v  for c in range(0,3) for r in range(0,3)]),
-                    sense=plp.LpConstraintEQ, rhs=v, name=f"constraint_uniq_grid_{g}_{v}"))
-        
-        for r in rows:
-            for c in cols:
-                if(game[r][c] != 0):
-                    prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid[r][c][v]*v for v in values]),
-                        sense=plp.LpConstraintEQ, rhs = game[r][c], name=f"constraint_prefilled_{r}_{c}"))
+            for value in values:
+                prob.addConstraint(plp.LpConstraint(e=plp.lpSum([grid_vars[grid_row*3+row][grid_col*3+col][value]*value  for col in range(0,3) for row in range(0,3)]),
+                                            sense=plp.LpConstraintEQ, rhs=value, name=f"constraint_uniq_grid_{grid}_{value}"))
     
     def collect(self, grid, rows, cols, values):
         sol = [[0 for c in cols] for r in rows]
@@ -71,4 +65,4 @@ class Linear:
                     if plp.value(grid[r][c][v]):
                         sol[r][c] = v
         return sol
-                    
+                        
--- Sudoku.py
+++ Sudoku.py
@@ -11,5 +11,14 @@ if __name__ == "__main__":
     sudoku.populate(12)
     print(sudoku)
     
-    s = SimpleSolver(sudoku)
+    # Using Crook's
+    # s = SimpleSolver(sudoku)
+    # print(sudoku)
+
+    # Using Linear Programming
+
+    l = Linear(sudoku)
+    l.solve()
+    l.apply_solution()
     print(sudoku)
+    
